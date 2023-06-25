101f1e59aae9685070ef1dd5d58b203054c6c7b6
Kirill <kirill.zayats.99@mail.ru>
2023-06-25 15:18:43 +0300

    add: task 2-8

---
+++ script.js
@@ -0,0 +1,72 @@
+ let a1 = 5 % 3,
+    a2 = 3 % 5,
+    a3 = 5 + '3',
+    a4 = '5' - 3,
+    a5 = 75 + 'кг',
+    a6 = 785 * 653,
+    a7 = 100 / 25,
+    a8 = 0 * 0,
+    a9 = 0 / 2,
+    a10 = 89 / 0,
+    a11 = 98 + 2,
+    a12 = 5 - 98,
+    a13 = (8 + 56 * 4) / 5,
+    a14 = (9 - 12) * 7 / (5 + 2),
+    a15 = +"123",
+    a16 = 1 || 0,
+    a17 = false || true,
+    a18 = true > 0
+
+let width = 10
+let height = 23
+let SPryam = width * height
+
+let h = 10
+let VCilindra = Math.PI * h * Math.pow(a7 , 2) / 4 
+
+let r = 5
+let SKruga = Math.PI * Math.pow(5, 2)
+
+let a = 5
+let b = 7
+let STrap = (a + b) * h / 2
+
+let S = 2
+let p = 0.1
+let years = 5
+let Pereplata = S * p * years
+
+let xa = 8
+let xb = 3
+let x1 = (16 - xa + 2 * xb) / 2
+let x2 = (xa - 15 * xb) / (xb - 6)
+let x3 = 23780 / (3 + xa + xb)
+
+console.log("Задание №8\nx1 = " + x1 + "\n" + 
+                         "x2 = " + x2.toFixed(2) + "\n" + 
+                         "x3 = " + x3.toFixed(2))
+
+console.log("Задание №7\nПереплата = " + Pereplata + " млн.руб.") 
+console.log("Задание №6\nПлощать трапеции = " + STrap + " см2") 
+console.log("Задание №5\nПлощать круга = " + SKruga.toFixed(2) + " см2") 
+console.log("Задание №4\nОбъем цилиндра = " + VCilindra.toFixed(2) + " см3") 
+console.log("Задание №3\nПлощадь прямоугольника = " + SPryam + " см2") 
+console.log("Задание №2\n" + 
+            "a1 = " + a1 + "\n" +
+            "a2 = " + a2 + "\n" +
+            "a3 = " + a3 + "\n" +
+            "a4 = " + a4 + "\n" +
+            "a5 = " + a5 + "\n" +
+            "a6 = " + a6 + "\n" +
+            "a7 = " + a7 + "\n" +
+            "a8 = " + a8 + "\n" +
+            "a9 = " + a9 + "\n" +
+            "a10 = " + a10 + "\n" +
+            "a11 = " + a11 + "\n" +
+            "a12 = " + a12 + "\n" +
+            "a13 = " + a13 + "\n" +
+            "a14 = " + a14 + "\n" +
+            "a15 = " + a15 + "\n" +
+            "a16 = " + a16 + "\n" +
+            "a17 = " + a17 + "\n" +
+            "a18 = " + a18 + "\n")
