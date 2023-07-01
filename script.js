506bc6436899133b0313883c654e1bbd0ac51228
Kirill <kirill.zayats.99@mail.ru>
2023-07-01 14:08:09 +0300

    add: all task

--- script.js
+++ script.js
@@ -1,193 +1,201 @@
-//задание 1, 2
+//Задание #1
 {
-    let nameUser = prompt("Ваше имя");
-    let age = Number(prompt("Ваш возраст"));
-    let city = prompt("Ваш город проживания");
-    let phone = prompt("Ваш номер телефона");
-    let email = prompt("Ваша эл. почта");
-    let company = prompt("Название вашей компании");
-    document.write(`<h2>Задание №1.\nМеня зовут ${nameUser}. Мне ${age} лет. Я проживаю в
-    городе ${city} и работаю в компании ${company}. Мои контактные данные:
-    ${phone}, ${email}. </h2>`);
+    let allNumberUp = '';
+    let allNumberDown = '';
 
-    document.write(`<h2>Задание №2. ${nameUser} родился в ${new Date().getFullYear() - age} году </h2>`);
+    for (let index = 1; index <= 52; index++) {
+        allNumberUp += index + ' '; 
+    }
+    for (let index = 35; index >= 8; index--) {
+        allNumberDown += index + ' ';   
+    }
+    document.write(`<h2>Задание №1<br/>
+                        Вывод чисел от 1 до 52: ${allNumberUp}<br/>
+                        Вывод чисел от 35 до 8: ${allNumberDown}</h2>`)
 }
 
-//задание 3
+//Задание #2
 {
-    let check = true
-    let number;
-    while(check) {
-        number = Number(prompt("Введите шестизначное число"))
-        if(String(number).length == 6) {
-            check = false;
+    let allNumber = '',
+        index = 89;
+    while(index >= 11) {
+        allNumber += index + "<br/>"
+        index--
+    }
+    document.write(`<h2>Задание №2<br/>
+                        Вывод чисел от 89 до 11:<br>
+                        ${allNumber}</h2>`)   
+}
+
+//Задание #3 
+{
+    let sumNumber = 0;
+    let startInterval = 0;
+    let endInterval = 100;
+    for (let index = startInterval; index <= endInterval; index++) {
+        sumNumber += index;
+    }
+    document.write(`<h2>Задание №3<br/>
+                        Сумма чисел от ${startInterval} до ${endInterval} = ${sumNumber}</h2>`)  
+}
+
+//Задание #4
+{
+    let strSumNumber = '';
+    let startInterval = 1;
+    let endInterval = 5;
+    let sumNumber = 0;
+    for (let i = startInterval; i <= endInterval; i++) {
+        for (let j = 0; j <= i; j++) {
+            sumNumber += j;
+        }
+        strSumNumber += sumNumber + " ";
+        sumNumber = 0;
+    }
+    console.log(strSumNumber);
+    document.write(`<h2>Задание №4<br/>
+                        Сумма чисел каждого числа от ${startInterval} до ${endInterval}: ${strSumNumber}</h2>`) 
+}
+
+//Задание #5
+{
+     let startInterval = 8;
+     let endInterval = 89;
+     let strNumbersFor = '';
+     let strNumbersWhile = '';
+     for (let index = startInterval; index <= endInterval; index++) {
+        if(index % 2 === 0) {
+            strNumbersFor += index + " ";
+        }
+     }
+     while (startInterval <= endInterval) {
+        if(startInterval % 2 === 0) {
+            strNumbersWhile += startInterval + " ";
+        }
+        startInterval++;
+     }
+
+     document.write(`<h2>Задание №5<br/>
+                         Чётные числа (for): ${strNumbersFor}<br/>
+                         Чётные числа (while): ${strNumbersWhile}</h2>`)
+}
+
+//Задание #6
+{
+     let startInterval = 2;
+     let endInterval = 10;
+     let str = '';
+     for (let i = startInterval; i <= endInterval; i++) {
+        for (let j = 1; j <= endInterval; j++) {
+            str += i + '*' + j + "=" + (i * j) + '<br/>'
+        }
+        str += '<br/>'      
+     }
+     document.write(`<h2>Задание №6<br/>
+                         Таблица умножения: <br/> 
+                         ${str}</h2>`) 
+}
+
+//Задание #7
+{
+    let n = 1000;
+    let num = 0;
+    let numberEnd = 50;
+    let dividerNumber = 2;
+    for (n; n > numberEnd; n = n /dividerNumber) {
+        num++; 
+    }
+    document.write(`<h2>Задание №7<br/>
+                        Число = ${n}<br/> 
+                        Число итераций = ${num}</h2>`) 
+}
+
+//Задание #8
+{
+    let isWrite = true;
+    let sum = 0;
+    let index = 1;
+    while(isWrite) {
+        let inputNumber = prompt(`Введите число ${index}`);
+        if(inputNumber == '' || inputNumber == 0) {
+            isWrite = false;
         }
         else {
-            alert(`Введенное число должно иметь 6 цифер`);
+            if(typeof Number(inputNumber) === 'number' && !isNaN(inputNumber)) {
+                sum += Number(inputNumber);
+                index++;
+            }
+            else {
+                alert("Ошибка - введеное вами значение не удалось преобразовать в число." + 
+                "Если хотите продолжить введите число или пустое значение/0 чтобы остановить");
+            }
         }
     }
-    let sumFirst = String(number).slice(0,String(number).length / 2)
-                             .split('')
-                             .reduce(function(previousValue, currentValue) {
-                                    return Number(previousValue) + Number(currentValue);
-                             });
-    let sumLast = String(number).slice(String(number).length / 2, String(number).length)
-                            .split('')
-                            .reduce(function(previousValue, currentValue) {
-                                    return Number(previousValue) + Number(currentValue);
-                            });
-    let answer = "нет"
-    if(sumLast === sumFirst) {
-        answer = "да"
-    }
-    document.write(`<h2>Задание №3. ${answer}`);
+    document.write(`<h2>Задание №8<br/>
+                        Сумма = ${sum}<br/> 
+                        Кол-во чисел = ${index - 1}<br/> 
+                        Среднее арифметическое = ${sum / (index - 1)}</h2>`) 
 }
 
-//задание 4
+//Задание #9
 {
-    let a1 = 1;
-    let a2 = 0;
-    let a3 = -3;
-    const isCheck = a => {
-        return (a > 0) ? "верно" : (a === 0) ? "а равно 0" : "не верно";
-    
-    }
-    document.write(`<h2>Задание №4. <br>Если а = 1, то ${(isCheck(a1))}; <br>Если а = 0, то ${(isCheck(a2))}; <br>Если а = -3, то ${(isCheck(a3))};`);    
-}
+    let str = '4 98 4 6 1 32 4 65 -22 4 3 5 7 89 7 10 1 36 8 57 -1'
+    let minValue, maxValue,
+        firstNumberIndex = 0;
+    for (let index = 0; index < str.length; index++) {
+        let isStatus = true;
+        let number = str[index];
 
-//задание 5
-{
-    let a = 10;
-    let b = 2;
-    document.write(`<h2>Задание №5. 
-                    <br>Сумма = ${(a + b) > 1 ? Math.pow(a + b, 2) : a + b}; 
-                    <br>Разность = ${a - b}; 
-                    <br>Произведение = ${a * b};
-                    <br>Частное = ${a / b};`
-                    );    
-}
-
-//задание 6
-{
-    let a = prompt("Введите число a")
-    let b = prompt("Введите число b")
-    document.write(`<h2>Задание №6. 
-                    <br> ${(a > 2 && a < 11) || (b >= 6 && b < 14) ? "Верно" : "Неверно" }`);    
-}
-
-//задание 7
-{
-    let minute = Number(prompt("Введите кол-во минут"))
-    let part = Math.ceil((minute / (60/4)))
-    document.write(`<h2>Задание №7. 
-                    <br>Четверть часа: ${part === 0 ? 1 : part}`);  
-}
-
-const year = 365;
-const month = 31;
-const week = 7;
-const hours = 24;
-const minuteOrSecond = 60;
-
-//задание 8, 10
-{
-    let check = true
-    let day;
-    while(check) {
-        day = Number(prompt("Введите день"));
-        if(day < 1 || day > year) {
-            alert(`значение дня должно быть положительным, но меньше дней в году (${year})`);
-        } else {
-            check = false;
+        while (isStatus) {
+                if(index < str.length - 1 && str[index + 1].trim() != '' ) {
+                    number += str[index + 1];
+                } else {
+                    isStatus = false;
+                }
+                index++;
         }
-        
-    }
-    let decada = Math.ceil((day % month / 10))
-    document.write(`<h2>Задание №8. 
-                    <br>Декада номер ${day === month ? 3 : decada}`); 
-    let monthNow = Math.ceil(day / month)   
-    let timeYear = ''
-    switch (monthNow) {
-        case 1:
-        case 2:
-        case 12:
+        if(!firstNumberIndex) {
+            minValue = maxValue = number;
+            firstNumberIndex++;
+            
+        }
+        else {
 
-            timeYear = "зима"
-            break;
-        case 3:
-        case 4:
-        case 5:
-            timeYear = "весна"
-            break;
-        case 6:
-        case 7:
-        case 8:
-            timeYear = "лето"
-            break;
-        case 9:
-        case 10:
-        case 11:
-            timeYear = "осень"
-            break;
-        default:
-            timeYear = " неудалось определить пору года"
-            break;
+            minValue = (Number(minValue) > Number(number)) ? number : minValue
+            maxValue = (Number(maxValue) < Number(number)) ? number : maxValue
+        }
     }
-    document.write(`<h2>Задание №10. 
-    <br>Месяц номер ${monthNow}
-    <br>Пора года ${timeYear}`);  
+    document.write(`<h2>Задание №9<br>
+                    Минимальное значение равно  = ${minValue}<br>
+                    Максимальное значение равно = ${maxValue}</h2>`)
 }
 
-//задание 9
+//Задание #10 
 {
-    let day = Number(prompt("Введите кол-во дней"))
-    let countYears;
-    let countMonth;
-    let countWeek;
-    let countHours;
-    let countMinute;
-    let countSecond;
-    if(day >= year) {
-        countYears = Math.floor(day / year);
-        day = day % year;
-    } else {
-        countYears = "Меньше года";
+    let isWrite = true;
+    let inputNumber;
+    while(isWrite) {
+        strInputNumber = prompt("Введите число!")
+        inputNumber = strInputNumber[0] == "-" ? -1 * Number(strInputNumber) : Number(strInputNumber)
+        if(typeof inputNumber === 'number' && 
+            !isNaN(inputNumber) && 
+            (inputNumber ^ 0) === inputNumber &&
+            strInputNumber != undefined) {
+            isWrite = false;
+        }
+        else {
+            alert("Ошибка - введеное вами значение не удалось преобразовать в число." + 
+                  "Если хотите продолжить введите число");
+        }
     }
-    if(day >= month) {
-        countMonth = Math.floor(day / month);
-        day = day % month;
-    } else {
-        countMonth = "Меньше месяца";
+    inputNumber = String(inputNumber)
+    let sum = 0;
+    for (let i = 0; i < inputNumber.length; i++) {
+        sum += Number(inputNumber[i]);       
     }
-    if(day >= week) {
-        countWeek = Math.floor(day / week);
-        day = day % week;
-    } else {
-        countWeek = "Меньше недели";
-    }
-    if(day > 0) {
-        countHours = Math.floor(day * hours);
-        day = day * hours - countHours;
-    } else {
-        countHours = "Меньше часа";
-    }
-    if(day > 0) {
-        countMinute = Math.floor(day * minuteOrSecond);
-        day = day * minuteOrSecond - countMinute;
-    } else {
-        countMinute = "Меньше минуты";
-    }
-    if(day > 0) {
-        countSecond = Math.floor(day * minuteOrSecond);
-        day = day * minuteOrSecond - countSecond;
-    } else {
-        countSecond = "Меньше секунды";
-    }
-    document.write(`<h2>Задание №9. 
-                    <br>Года: ${countYears}
-                    <br>Месяцев: ${countMonth}
-                    <br>Недель: ${countWeek} 
-                    <br>Часов: ${countHours}
-                    <br>Минут: ${countMinute}
-                    <br>Секунд: ${countSecond}`);  
+    document.write(`<h2>Задание №10<br>
+                Цифры: ${inputNumber.split('').join(' ')}<br/>
+                Кол-во цифр = ${inputNumber.length}<br/>
+                Сумма = ${sum}<br/>
+                Обратный порядок: ${inputNumber.split('').reverse().join('')}</h2>`)
 }
