caedaf04a57d13ea7f42de128d2185df792d336d
Kirill <kirill.zayats.99@mail.ru>
2023-07-04 21:17:59 +0300

    add: 11 tasks

--- script.js
+++ script.js
@@ -1,201 +1,199 @@
 //Задание #1
 {
-    let allNumberUp = '';
-    let allNumberDown = '';
+    let numbers = [1, 2, 3, 4, 5];
 
-    for (let index = 1; index <= 52; index++) {
-        allNumberUp += index + ' '; 
-    }
-    for (let index = 35; index >= 8; index--) {
-        allNumberDown += index + ' ';   
-    }
     document.write(`<h2>Задание №1<br/>
-                        Вывод чисел от 1 до 52: ${allNumberUp}<br/>
-                        Вывод чисел от 35 до 8: ${allNumberDown}</h2>`)
+                    Вывод массива:</h2>`)
+    for (let index = 0; index < numbers.length; index++) {
+        document.write(`<h3>${numbers[index]}</h3>`) 
+    }
 }
 
 //Задание #2
 {
-    let allNumber = '',
-        index = 89;
-    while(index >= 11) {
-        allNumber += index + "<br/>"
-        index--
+    let numbers = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7];
+    document.write(`<h2>Задание №2<br/>Вывод отрицательных чисел от -10 до -3</h2>`)
+    for (let index = 0; index < numbers.length; index++) {
+        if(numbers[index] > -10 && numbers[index] < -3) {
+            document.write(`<h3>${numbers[index]}</h3>`) 
+        }
     }
-    document.write(`<h2>Задание №2<br/>
-                        Вывод чисел от 89 до 11:<br>
-                        ${allNumber}</h2>`)   
 }
 
 //Задание #3 
 {
-    let sumNumber = 0;
-    let startInterval = 0;
-    let endInterval = 100;
+    let numbersFor = [];
+    let numbersWhile = [];
+
+    let startInterval = 23;
+    let endInterval = 57;
+    let result = 0;
+
     for (let index = startInterval; index <= endInterval; index++) {
-        sumNumber += index;
+        numbersFor.push(index);      
     }
-    document.write(`<h2>Задание №3<br/>
-                        Сумма чисел от ${startInterval} до ${endInterval} = ${sumNumber}</h2>`)  
+    let index = startInterval;
+    while(index <= endInterval) {
+        numbersWhile.push(index)
+        index++;
+    }
+    for (let index = 0; index < numbersFor.length; index++) result += numbersFor[index];
+
+    document.write(`<h2>Задание №3</h2>
+                    <h3>Вывод массивов:
+                    <br/>Массив numbersFor = ${numbersFor}
+                    <br/>Массив numbersWhile = ${numbersWhile}
+                    <br/>Сумма элментов result = ${result}</h3>`)
 }
 
 //Задание #4
 {
-    let strSumNumber = '';
-    let startInterval = 1;
-    let endInterval = 5;
-    let sumNumber = 0;
-    for (let i = startInterval; i <= endInterval; i++) {
-        for (let j = 0; j <= i; j++) {
-            sumNumber += j;
-        }
-        strSumNumber += sumNumber + " ";
-        sumNumber = 0;
+    let massive = ['10', '20', '30', '50', '235', '3000'];
+    document.write(`<h2>Задание №4 <br/> Числа на цифру 1, 2, 5:</h2>`);
+    for (let index = 0; index <= massive.length; index++) {
+        let value = String(massive[index])[0];
+        value == 1 ||
+        value == 2 ||
+        value == 5 ? document.write(`<h3>${massive[index]}</h3>`) : 0;
     }
-    console.log(strSumNumber);
-    document.write(`<h2>Задание №4<br/>
-                        Сумма чисел каждого числа от ${startInterval} до ${endInterval}: ${strSumNumber}</h2>`) 
 }
 
 //Задание #5
 {
-     let startInterval = 8;
-     let endInterval = 89;
-     let strNumbersFor = '';
-     let strNumbersWhile = '';
-     for (let index = startInterval; index <= endInterval; index++) {
-        if(index % 2 === 0) {
-            strNumbersFor += index + " ";
-        }
-     }
-     while (startInterval <= endInterval) {
-        if(startInterval % 2 === 0) {
-            strNumbersWhile += startInterval + " ";
-        }
-        startInterval++;
-     }
-
-     document.write(`<h2>Задание №5<br/>
-                         Чётные числа (for): ${strNumbersFor}<br/>
-                         Чётные числа (while): ${strNumbersWhile}</h2>`)
+    let massive = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС'];
+    document.write(`<h2>Задание №5 <br/> Дни недели:</h2>`);
+    for (let index = 0; index < massive.length; index++) {
+        index > 4 ? document.write(`<b>${massive[index]}</b> `) : 
+                             document.write(`${massive[index]} `);
+    }
 }
 
 //Задание #6
 {
-     let startInterval = 2;
-     let endInterval = 10;
-     let str = '';
-     for (let i = startInterval; i <= endInterval; i++) {
-        for (let j = 1; j <= endInterval; j++) {
-            str += i + '*' + j + "=" + (i * j) + '<br/>'
-        }
-        str += '<br/>'      
-     }
-     document.write(`<h2>Задание №6<br/>
-                         Таблица умножения: <br/> 
-                         ${str}</h2>`) 
+    let oldMassive = ['ПН', 1234, '456', false, 'Купить', 15];
+    let massive = [];
+    Array.prototype.push.apply(massive, oldMassive);
+
+    massive.push(massive[Math.floor(Math.random() * massive.length)]);
+    document.write(`<h2>Задание №6<br/>
+                    Начальный массив: ${oldMassive.toString()}<br/>
+                    Полученный массив: ${massive.toString()}<br/>
+                    Последний элемент:
+                    ${massive[massive.length - 1]}</h2>`);
+
 }
 
 //Задание #7
 {
-    let n = 1000;
-    let num = 0;
-    let numberEnd = 50;
-    let dividerNumber = 2;
-    for (n; n > numberEnd; n = n /dividerNumber) {
-        num++; 
-    }
-    document.write(`<h2>Задание №7<br/>
-                        Число = ${n}<br/> 
-                        Число итераций = ${num}</h2>`) 
-}
-
-//Задание #8
-{
-    let isWrite = true;
-    let sum = 0;
+    let massive = [];
     let index = 1;
-    while(isWrite) {
+    while(1) {
         let inputNumber = prompt(`Введите число ${index}`);
-        if(inputNumber == '' || inputNumber == 0) {
-            isWrite = false;
+        if(inputNumber == '') {
+            break;
         }
         else {
-            if(typeof Number(inputNumber) === 'number' && !isNaN(inputNumber)) {
-                sum += Number(inputNumber);
+            if(!isNaN(inputNumber)) {
+                massive.push(inputNumber)
                 index++;
             }
             else {
                 alert("Ошибка - введеное вами значение не удалось преобразовать в число." + 
-                "Если хотите продолжить введите число или пустое значение/0 чтобы остановить");
+                "Если хотите продолжить введите число или пустое значение чтобы остановить");
             }
         }
     }
+    document.write(`<h2>Задание №7 <br/>Полученный массив: ${massive}</h2>`);
+}
+
+// //Задание #8
+{
+    let massive = [12, false, 'Текст', 4, 2, -5, 0, 2]; 
+    let massiveReverse = [];
+    Array.prototype.push.apply(massiveReverse, massive);
+
+    let index = 0;
+    let lastIndex = massive.length - 1;
+    while (index < (massive.length) / 2) {
+        [massiveReverse[index], massiveReverse[lastIndex]] = [massiveReverse[lastIndex], massiveReverse[index]];
+        index++;
+        lastIndex--;
+    }
     document.write(`<h2>Задание №8<br/>
-                        Сумма = ${sum}<br/> 
-                        Кол-во чисел = ${index - 1}<br/> 
-                        Среднее арифметическое = ${sum / (index - 1)}</h2>`) 
+                    Массив: ${massive}<br/>
+                    В обратном порядке (reverse): ${massive.reverse()}<br/>
+                    В обратном порядке (while): ${massiveReverse}</h2>`);
 }
 
 //Задание #9
 {
-    let str = '4 98 4 6 1 32 4 65 -22 4 3 5 7 89 7 10 1 36 8 57 -1'
-    let minValue, maxValue,
-        firstNumberIndex = 0;
-    for (let index = 0; index < str.length; index++) {
-        let isStatus = true;
-        let number = str[index];
-
-        while (isStatus) {
-                if(index < str.length - 1 && str[index + 1].trim() != '' ) {
-                    number += str[index + 1];
-                } else {
-                    isStatus = false;
-                }
-                index++;
-        }
-        if(!firstNumberIndex) {
-            minValue = maxValue = number;
-            firstNumberIndex++;
-            
-        }
-        else {
-
-            minValue = (Number(minValue) > Number(number)) ? number : minValue
-            maxValue = (Number(maxValue) < Number(number)) ? number : maxValue
-        }
-    }
-    document.write(`<h2>Задание №9<br>
-                    Минимальное значение равно  = ${minValue}<br>
-                    Максимальное значение равно = ${maxValue}</h2>`)
+let massive = [,5, 9, 21, , , 9, 78, , , , 6];
+let count = 0;
+for (const item of massive) isNaN(item) ? count++ : 0;
+document.write(`<h2>Задание №9<br/>
+                Массив: ${massive}<br/>
+                Кол-во пустых значений count = ${count}</h2>`);
 }
 
 //Задание #10 
 {
-    let isWrite = true;
-    let inputNumber;
-    while(isWrite) {
-        strInputNumber = prompt("Введите число!")
-        inputNumber = strInputNumber[0] == "-" ? -1 * Number(strInputNumber) : Number(strInputNumber)
-        if(typeof inputNumber === 'number' && 
-            !isNaN(inputNumber) && 
-            (inputNumber ^ 0) === inputNumber &&
-            strInputNumber != undefined) {
-            isWrite = false;
-        }
-        else {
-            alert("Ошибка - введеное вами значение не удалось преобразовать в число." + 
-                  "Если хотите продолжить введите число");
-        }
-    }
-    inputNumber = String(inputNumber)
+    let massive = [48,9,0,4,21,2,1,0,8,84,76,8,0,13,2]
+    let startIndex = massive.indexOf(0);
+    let endIndex = massive.lastIndexOf(0);
     let sum = 0;
-    for (let i = 0; i < inputNumber.length; i++) {
-        sum += Number(inputNumber[i]);       
+    if(startIndex != -1 && endIndex != -1 && startIndex != endIndex) {
+        for (let index = startIndex; index < endIndex; index++) {
+            sum += massive[index];
+        }
     }
-    document.write(`<h2>Задание №10<br>
-                Цифры: ${inputNumber.split('').join(' ')}<br/>
-                Кол-во цифр = ${inputNumber.length}<br/>
-                Сумма = ${sum}<br/>
-                Обратный порядок: ${inputNumber.split('').reverse().join('')}</h2>`)
+    document.write(`<h2>Задание №10<br/>
+                Массив: ${massive}<br/>
+                Сумма sum = ${sum}</h2>`);
+}
+
+
+let initMassive = (height, weight) => {
+    let massive = new Array(height);
+    for (let rowIndex = 0; rowIndex < height; rowIndex++) {
+        massive[rowIndex] = new Array(weight);
+        for (let columnIndex = 0; columnIndex < weight; columnIndex++) {
+            massive[rowIndex][columnIndex] = '_';
+        }      
+    }
+    return massive;
+}
+
+//Задание #11
+{
+    let minLenght = 3;
+    let valuePyramid = '^';
+    while(1) {
+        heightPyramid = Number(prompt(`Введите высоту пирамиды`));
+        if(!isNaN(heightPyramid) && heightPyramid >= minLenght) {
+            break;
+        } else if(heightPyramid < minLenght) {
+            alert(`Минимальная высота пирамиды равна ${minLenght}`);
+        } else {
+            alert("Что то пошло не так. Введите число.");
+        }
+    }
+    let weightPyramid = 2 * heightPyramid - 1;
+    let massive = initMassive(heightPyramid, weightPyramid);
+    let startIndexWeight = 0;
+    let endIndexWeight = weightPyramid;
+    for (let row = heightPyramid - 1; row >= 0; row--) {
+        for (let column = startIndexWeight; column < endIndexWeight; column++) {  
+            massive[row][column] = valuePyramid;     
+        }        
+        startIndexWeight++;
+        endIndexWeight--;
+    }
+
+    document.write(`<h2>Задание №11 <br/>Пирамида:</h2>`);
+    for (let rowIndex = 0; rowIndex < heightPyramid; rowIndex++) {
+        for (let columnIndex = 0; columnIndex < weightPyramid; columnIndex++) {
+            document.write(`${massive[rowIndex][columnIndex]}`);
+        }      
+        document.write(`<br/>`);
+    }
 }
